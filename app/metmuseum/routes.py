c064aa1f304c93630f5ffa03e85922b5dbf38642
frantiseksereda <frantisek.sereda@gmail.com>
2023-12-30 20:37:45 +0100

    met museum api seznam oddělení

--- app/metmuseum/routes.py
+++ app/metmuseum/routes.py
@@ -1,7 +1,23 @@
 from flask import Flask, request, render_template
 from app.metmuseum import bp
 import pandas as pd
+import http.client
+import json
+
+
+def get_met_departments_table():
+    conn = http.client.HTTPSConnection("collectionapi.metmuseum.org")
+    payload = ''
+    headers = {}
+    conn.request("GET", "/public/collection/v1/departments", payload, headers)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    json_data = json.loads(data)
+    departments_df = pd.DataFrame(json_data['departments'])
+    departments_table_html = departments_df.to_html()
+    return departments_table_html
+    
 
 @bp.route("/metmuseum")
 def show_met_api():
-    return render_template("metmuseum/metmuseum.html")
+    return render_template("metmuseum/metmuseum.html", departments=get_met_departments_table())
--- app/pokusy.py
+++ app/pokusy.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import base64
+import http.client
+import json
 
 data = pd.DataFrame({
     'Start Time': ['2022-01-01 09:00:00', '2022-01-01 12:30:00', '2022-01-01 14:45:00'],
@@ -22,3 +24,12 @@ combined_data.sort_values(inplace=True)
 
 print(combined_data)
 
+conn = http.client.HTTPSConnection("collectionapi.metmuseum.org")
+payload = ''
+headers = {}
+conn.request("GET", "/public/collection/v1/departments", payload, headers)
+res = conn.getresponse()
+data = res.read().decode("utf-8")
+json_data = json.loads(data)
+departments_df = pd.DataFrame(json_data['departments'])
+print(departments_df)
